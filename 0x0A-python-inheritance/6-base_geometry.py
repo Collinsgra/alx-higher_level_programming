cdb21b8bf2b7ece0ad26e60d7ed2b282969710ed
Collinsgra <kipkemoicollins941@gmail.com>
2023-06-12 13:31:41 -0700

    Base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines class = BaseGeometry."""
+
+
+class BaseGeometry:
+    """class rep geometry"""
+
+    def area(self):
+        """yet to add this method"""
+        raise Exception("area() is not implemented")
