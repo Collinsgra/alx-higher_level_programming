fa21c57e2f74390d1cb19f8bf816a9ecdff226b2
Collinsgra <kipkemoicollins941@gmail.com>
2023-06-13 10:27:30 -0700

    Log parsing

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""read stdin and determine
+"""
+
+
+def prntStatics(size, status_codes):
+    """Print avail metrics
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
+
+
+if __name__ == "__main__":
+    import sys
+
+    size = 0
+    status_codes = {}
+    possible_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                prntStatics(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in possible_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        prntStatics(size, status_codes)
+
+    except KeyboardInterrupt:
+        prntStatics(size, status_codes)
+        raise
