9d5c7517dbb77b66204a8b36223fe11bd2794621
Collinsgra <kipkemoicollins941@gmail.com>
2023-07-20 14:07:11 -0700

    :10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,5 @@
---lists all records of the table second_table
-SELECT score, name FROM second_table ORDER BY score DESC;
+-- lists all records of the table second_table
+
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
