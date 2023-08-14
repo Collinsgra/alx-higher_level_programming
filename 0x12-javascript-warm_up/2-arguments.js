74ce2555075896de187c81c07a50f331a8b9cded
Collinsgra <kipkemoicollins941@gmail.com>
2023-08-14 12:43:04 -0700

    2 arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const argsNumb = process.argv.length - 2;
 if (argsNumb === 0) {
-	console.log("No argument");
+	console.log('No argument');
 } else if ( argsNumb === 1) {
-	console.log("Argument found");
+	console.log('Argument found');
 } else {
-	console.log("Arguments found");
+	console.log('Arguments found');
 }
